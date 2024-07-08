b5bd9600c2a9d77ac9053b6f433ba55f5f51e8af
tommydebisi <tommydebisi@gmail.com>
2024-07-08 21:22:25 +0100

    :chore: more test

--- db/sqlc/store_test.go
+++ db/sqlc/store_test.go
@@ -1,11 +1,26 @@
 package db
 
 import (
+	"context"
 	"testing"
 )
 
 func TestTransferTx(t *testing.T) {
 	store := NewStore(testDb)
 
-	transfer := createRandTransfer(t)
+	// create transfers to send
+	transfer := createRandTransfer(t, createRandomAccount(t).ID, createRandomAccount(t).ID)
+	transfer2 := createRandTransfer(t, createRandomAccount(t).ID, createRandomAccount(t).ID)
+
+	// create channel
+	// run series of transfer transactions concurrently
+	for i := 0; i < 5; i++ {
+		go func() {
+			store.TransferTx(context.Background(), TransferTxParams{
+				FromAccountId: transfer.ID,
+				ToAccountId:   transfer2.ID,
+				Amount:        10,
+			})
+		}()
+	}
 }
